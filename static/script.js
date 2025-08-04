6e753acefd9e2230310358bd8d18d7ea10a9d0ff
MeghanathMC <meghanath.mc24@gmail.com>
2025-08-04 21:20:19 +0530

    day 3 done

--- static/script.js
+++ static/script.js
@@ -1,3 +1,38 @@
 function sayHello() {
     alert("Hello - We are building an Voice AI Agent!");
 }
+
+async function generateAudio(){
+    const textInput=document.getElementById("inputText").value;
+
+   const audioPlayer= document.getElementById("audioPlayer");
+
+
+    if(textInput.trim() === ""){
+        alert("Please enter some text to generate audio");
+        return;
+    }
+
+    const response= await fetch("/tts",{
+        method:"POST",
+        headers:{
+            "Content-Type":"application/json",
+
+        },
+        body:JSON.stringify({text:textInput})
+    })
+
+   const data=await response.json();
+
+
+   if(data.audio_url && data.audio_url!=="Not Found"){
+    audioPlayer.src=data.audio_url;
+    audioPlayer.style.display="block";
+    audioPlayer.play();
+   
+   }else{
+    alert("Failed to generate audio");
+   }
+
+
+}
